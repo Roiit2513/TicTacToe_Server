80c5fc3436c368c7fdcbaec0d8f287def1dc8dea
Rohit <patilrohit2513@gmail.com>
2023-07-02 13:35:52 +0530

    updated CORS

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://tictactoe-mskw.onrender.com",
         methods: ["GET", "POST"],
     }
 })
